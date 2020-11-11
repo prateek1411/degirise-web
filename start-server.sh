607635c9b9768f02b1ec5a94a65f72bebdc0f034
prateek.srivastava <Prateek1411@gmail.com>
2020-11-11 16:20:25 +0100

    Fixed start-server.sh

--- start-server.sh
+++ start-server.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 # start-server.sh
-if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
-	    (cd digiriseWeb; python manage.py createsuperuser --no-input)
-fi
 (cd digiriseWeb; gunicorn digiriseWeb.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3) &
-nginx -g "daemon off;"
