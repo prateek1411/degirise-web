8e87957aad05bde98b985adb6e23e2f59184702b
prateek.srivastava <Prateek1411@gmail.com>
2020-11-13 13:47:37 +0100

    Added Postgres database

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
 # Dockerfile
 FROM python:3.9-buster
-# install nginx
-RUN apt-get update && apt-get install nginx vim -y --no-install-recommends
-COPY nginx.default /etc/nginx/sites-available/default
-RUN ln -sf /dev/stdout /var/log/nginx/access.log \
-    && ln -sf /dev/stderr /var/log/nginx/error.log
-
 # copy source and install dependencies
+
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+RUN pip install --upgrade pip
+
 RUN mkdir -p /opt/app
 RUN mkdir -p /opt/app/pip_cache
 RUN mkdir -p /opt/app/digiriseWeb
@@ -14,10 +14,11 @@ RUN mkdir -p /opt/app/pip_cache
 COPY requirements.txt start-server.sh /opt/app/
 COPY digiriseWeb /opt/app/digiriseWeb/
 WORKDIR /opt/app
+
 RUN pip install -r requirements.txt --cache-dir /opt/app/pip_cache
 RUN chown -R www-data:www-data /opt/app
 
 # start server
-EXPOSE 8020
+EXPOSE 8010
 STOPSIGNAL SIGTERM
 CMD ["/bin/bash","/opt/app/start-server.sh"]
--- digiriseWeb/digiriseWeb/settings.py
+++ digiriseWeb/digiriseWeb/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'k5wgojir=p8ic=2*@snm&ob-zw8pih_#$h&x#d#t#b(t*#&ot8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['digirise.westeurope.cloudapp.azure.com','localhost','127.0.0.1']
 
@@ -74,13 +74,23 @@ WSGI_APPLICATION = 'digiriseWeb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
