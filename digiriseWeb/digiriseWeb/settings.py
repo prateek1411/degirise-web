7d9e7bc701081c75cfe741a28f7c600e55703950
prateek.srivastava <Prateek1411@gmail.com>
2021-01-18 10:12:24 +0100

    Added social Oauth

--- digiriseWeb/digiriseWeb/settings.py
+++ digiriseWeb/digiriseWeb/settings.py
@@ -133,9 +133,9 @@ LOGOUT_REDIRECT_URL = 'index'
 # config/settings.py
 #gmail_send/settings.py
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'prateek1411@gmail.com'
-EMAIL_HOST_PASSWORD = 'lkllttgqnaapkpcw' #past the key or password app here
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD') #past the key or password app here
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'default from email'
