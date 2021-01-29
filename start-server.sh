16a61613f9103f1cee558b8cbfe871af5172e052
prateek.srivastava <Prateek1411@gmail.com>
2021-01-29 17:17:17 +0100

    reload

--- start-server.sh
+++ start-server.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # start-server.sh
-cd digiriseWeb; gunicorn digiriseWeb.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3
+cd digiriseWeb; gunicorn digiriseWeb.wsgi --user www-data --bind 0.0.0.0:8010 --workers 1 --reload
 
