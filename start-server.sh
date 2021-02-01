4d02df31d716cbe4a81bb5a45e649a15ba5d31ba
prateek.srivastava <Prateek1411@gmail.com>
2021-02-01 23:10:02 +0100

    uwsgi instead of unicorn

--- start-server.sh
+++ start-server.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 # start-server.sh
-cd digiriseWeb; gunicorn digiriseWeb.wsgi --user www-data --bind 0.0.0.0:8010 --workers 1 --reload
+#cd digiriseWeb; gunicorn digiriseWeb.wsgi --user www-data --bind 0.0.0.0:8010 --workers 1 --reload
+uwsgi --socket 127.0.0.1:8010  --chdir digiriseWeb/ --wsgi-file digiriseWeb/wsgi.py --master --processes 2 --threads 2 --stats 127.0.0.1:9191
+
 
